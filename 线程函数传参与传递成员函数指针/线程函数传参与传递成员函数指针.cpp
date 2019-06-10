62b0c18723def21cc296da262e824fd7ec65c9eb
Lerchain <544918380@qq.com>
2019-06-10 18:57:02 +0800

    传函数指针

---
+++ 线程函数传参与传递成员函数指针/线程函数传参与传递成员函数指针.cpp
@@ -0,0 +1,47 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <string>
+using namespace std;
+
+class ThreadGuard
+{
+	thread& t;
+public:
+	explicit ThreadGuard(thread& _t):t(_t)
+	{
+	}
+	~ThreadGuard()
+	{
+		if (t.joinable())
+		{
+			t.join();
+		}
+	}
+	ThreadGuard(ThreadGuard&) = delete;
+	ThreadGuard& operator=(ThreadGuard&) = delete;
+};
+void func(const string& Aword)
+{
+	cout << Aword << endl;
+}
+class chuanzhizhen {
+public:
+	void eg(const string& s)
+	{
+		cout << s << endl;
+	}
+};
+int main()
+{
+	string s = "This is a word";
+	thread t(func, ref(s)); //ref 保证是真的传地址。
+	ThreadGuard g(t);
+	cout << "This is Main thread" << endl;
+	chuanzhizhen hehe;
+	string s1 = "This is eg\n";
+	thread t2(&chuanzhizhen::eg, &hehe, s1);
+	ThreadGuard k(t2);
+}
+
