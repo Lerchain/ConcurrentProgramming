025cadf8bb7ac9ffd5eb0ccb8fbac8b2ce76d0ef
Lerchain <544918380@qq.com>
2019-06-11 20:00:17 +0800

    运用move转移,vector量产

---
+++ 转移线程与量产线程/转移线程与量产线程.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <string>
+#include <vector>
+#include <stdio.h>
+using namespace std;
+
+class ThreadGuard
+{
+	thread t;
+public:
+	explicit ThreadGuard(thread _t) :t(move(_t))
+	{
+		if (!t.joinable())
+		{
+			throw "This thread is NULL";
+		}
+	}
+	~ThreadGuard()
+	{
+		t.join();
+	}
+	ThreadGuard(ThreadGuard&) = delete;
+	ThreadGuard& operator=(ThreadGuard&) = delete;
+};
+void Origin(const string& s)
+{
+	cout << s <<" Original thread" << endl;
+}
+void ThreadGuardMoveText(const string& s)
+{
+	cout << s << "Test" << endl;
+}
+void doSomething(int i)
+{
+	printf("%d ", i);
+	printf("do something");
+}
+int main()
+{
+	string s = "Hello";
+	thread t1(Origin, s);
+	thread t2 = move(t1);
+	t2.join();
+	cout << "This is main thread" << endl;
+	string s2 = "This is ThreadGuard move";
+	ThreadGuard g(thread(ThreadGuardMoveText,s2));
+	vector<thread> threads;
+	for (int i = 0; i < 20; i++)
+	{
+		threads.push_back(thread(doSomething, i));
+	}
+	for (int i = 0; i < 20; i++)
+	{
+		threads[i].join();
+	}
+}
+
+
