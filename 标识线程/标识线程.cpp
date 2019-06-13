809533b7e9429fba27f12121c590b5bfdc30dc25
Lerchain <544918380@qq.com>
2019-06-13 11:06:48 +0800

    thread::id类型标识，两种检索方式。

---
+++ 标识线程/标识线程.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <string>
+
+using namespace std;
+
+void doSomething(thread::id _id)
+{
+	if (this_thread::get_id() == _id)
+	{
+		cout << "I am doing something only in main thread" << endl;
+	}
+	cout << "I am doing something" << endl;
+}
+int main()
+{
+	thread::id master_id;
+	master_id = this_thread::get_id();
+	cout << "Master id is "<<master_id<<endl;
+	thread t(doSomething,ref(master_id));
+	t.join();
+	if (t.get_id() != master_id)
+		cout << "t thread id " << t.get_id() << " is not equal to master_id"<< endl;
+	doSomething(master_id);
+}
+
