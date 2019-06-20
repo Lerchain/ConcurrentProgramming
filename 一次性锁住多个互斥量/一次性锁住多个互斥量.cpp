917699895c7206a75eb72f45bf9ea39126f9220f
Lerchain <544918380@qq.com>
2019-06-20 13:05:18 +0800

    lock同时锁多个互斥元，死锁条件思索。

--- 一次性锁住多个互斥量/一次性锁住多个互斥量.cpp
+++ 一次性锁住多个互斥量/一次性锁住多个互斥量.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <map>
 #include <iterator>
+#include <vector>
 using namespace std;
 
 class someBigObject
@@ -68,20 +69,23 @@ public:
 	{
 		someDetail.showAll();
 	}
-	friend void swap(avoidDeadclock& lhs, avoidDeadclock& rhs)
-	{
-		lock(lhs.m, rhs.m);
-		lock_guard<mutex> lockA(lhs.m, adopt_lock);
-		lock_guard<mutex> lockB(rhs.m, adopt_lock);
-		swap(lhs.someDetail, rhs.someDetail);
-	}
-	friend void oops(avoidDeadclock& lhs, avoidDeadclock& rhs)
-	{
-		lock_guard<mutex> lockA(lhs.m);
-		lock_guard<mutex> lockB(rhs.m);
-		swap(lhs.someDetail, rhs.someDetail);
-	}
+	friend void safeSwap(avoidDeadclock& lhs, avoidDeadclock& rhs);
+	friend void oops(avoidDeadclock& lhs, avoidDeadclock& rhs);
+
 };
+void safeSwap(avoidDeadclock& lhs, avoidDeadclock& rhs)
+{
+	lock(lhs.m, rhs.m);
+	lock_guard<mutex> lockA(lhs.m, adopt_lock);
+	lock_guard<mutex> lockB(rhs.m, adopt_lock);
+	swap(lhs.someDetail, rhs.someDetail);
+}
+void oops(avoidDeadclock& lhs, avoidDeadclock& rhs)
+{
+	lock_guard<mutex> lockA(lhs.m);
+	lock_guard<mutex> lockB(rhs.m);
+	swap(lhs.someDetail, rhs.someDetail);
+}
 /*目前真没想出来怎么测试，实话说没有太看懂书上是什么意思，过些天再回头试试吧。就我目前的理解来说，死锁发生的情况在这个例子中是这样的：两个someBigObject实例，每个实例都
 在多个线程中不停setData,showData，那这就得保证一个线程setdata的时候另一个不能刚好show,也不能相反，所以需要互斥元，someBigObject中为了保证showAll与showName可以并行我用
 了两个互斥元，也不知道这样对不对，其实这时就已经可以直接运用lock锁两个互斥元了要不一不小心就死锁(L36)，而书中写的swap是想阐述一个问题就是能保证lock锁用对的情况下依然保
@@ -91,6 +95,8 @@ public:
 好像应该大概就是这么个意思吧。我可真是个小聪明鬼，好像给自己解释明白了。
 那我该怎么重现死锁方式呢？
 一个线程在顺序上锁的情况下进行swap交换应该都能重现吧
+
+我错了，死锁是得有两个线程，并且正好同时交换两个相同的实例，比如swap(Aa,Ba)和swap(Ba,Aa)并发了，这样的话前者锁住了Aa的锁需要Ba没锁，后者相反，造成死锁。
 */
 int main()
 {
@@ -104,19 +110,34 @@ int main()
 	avoidDeadclock Ba(B);
 	Aa.showAll();
 	Ba.showAll();
-	//A.showAll();
-	//B.showAll();
 	printf("\n");
-	oops(Aa, Ba);
-	//A.showAll();
-	//B.showAll();  经过测试我发现A，B没有交换，我意识到someBigobject应该仅仅就是存放数据的地方，交换的应该是avoidDeadclock的实例。所以我想到了让后者继承前者。
-	//Aa.showAll(); 
-	//Ba.showAll(); 继承后我发现没有任何输出，我才意识到父类的函数show的是父类的私有成员,不如把子类的成员去掉吧，都统一用父类的。
-	//然后我发现改起来好麻烦啊，我突然想到设计模式首要原则开闭原则，我觉得我不该这么改.
-	//但是我父类设计的其实就有点毛病，那应该被设计为存各种数据的地方。先写个show看看是不是交换过来了再说吧
+	//vector<thread> threads(10);
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	threads[i] = thread(oops, ref(Aa), ref(Ba));
+	//	threads[i + 1] = thread(oops, ref(Ba), ref(Aa));
+	//	i++;
+	//}
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	threads[i].join();
+	//}                               死锁！
+	vector<thread> threads(10);
+	for (int i = 0; i < 10; i++)
+	{
+		threads[i] = thread(safeSwap, ref(Aa), ref(Ba));
+		if (i == 8)
+			break;
+		threads[i + 1] = thread(safeSwap, ref(Ba), ref(Aa));  //我刚才i的值写错了，然后发现vector存thread越界不会报错，好像就是单纯的把越界的语句忽略了。
+		i++;
+	}
+	for (int i = 0; i < 9; i++)
+	{
+		threads[i].join();
+	}
 	Aa.showAll();
 	Ba.showAll();
-	//确实交换了，我知道没有死锁是因为没有交换mutex，因为mutex不允许拷贝或者赋值操作。
-	//不对，我理解错了，死锁是得有两个线程，并且正好同时交换两个相同的实例，比如swap(Aa,Ba)和swap(Ba,Aa)并发了，这样的话前者锁住了Aa的锁需要Ba的锁，后者相反，造成死锁。
-	//应该是这样的，明天再继续做吧。
+	
+
+
 }
