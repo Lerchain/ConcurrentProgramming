5d42120d2853f8064d988dc91d8d1946175cfaa6
Lerchain <544918380@qq.com>
2019-08-22 22:34:13 +0800

    构建细粒度锁的线程安全队列

---
+++ 构建细粒度锁的线程安全队列/构建细粒度锁的线程安全队列.cpp
@@ -0,0 +1,78 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+
+using namespace std;
+
+template<typename T>
+class SingleThreadQueue
+{
+	struct node
+	{
+		T data;
+		unique_ptr<node> next;
+		node(T _data):data(move(_data))
+		{}
+	};
+	unique_ptr<node> head;
+	node* tail;
+	//unique_ptr<node> tail;
+public:
+	SingleThreadQueue()
+	{}
+	
+	SingleThreadQueue(const SingleThreadQueue& other) = delete;
+	
+	SingleThreadQueue& operator=(const SingleThreadQueue& other) = delete;
+
+	shared_ptr<T> try_pop()
+	{
+		if (!head)
+		{
+		//	return nullptr;	//经测试，nullptr也行。
+		//	return NULL; 甚至NULL都可以。
+			return shared_ptr<T>();	
+		}
+		shared_ptr<T> const res(make_shared<T>(move(head->data)));
+		unique_ptr<node> const old_head = move(head);	//如果去掉上一行的move还勉强可以，只是多费点空间，这个move就不行了，unique_ptr不能拷贝，只能转移。
+		head = move(old_head->next);//同上
+		return res;
+	}
+	void push(T newValue)	//以下注释部分皆为tail设为unique_ptr，最低级错误示范。
+	{
+		unique_ptr<node> p(new node(move(newValue)));
+		node* const newTail = p.get();
+		//unique_ptr<node> newTail(new node(move(newValue)));
+		if (tail)
+		{
+			tail->next = move(p);
+		//	tail->next = move(newTail);
+		}
+		else
+		{
+			head = move(p);
+		//	node* const h = newTail.get();
+		//	head = move(h);		//失败在这里，如果队列本为空，push后头指针与尾指针皆应指向相同元素，unique_ptr又怎会有两个指针指向相同对象。
+		}
+		tail = newTail;
+//		tail = move(newTail);
+	}
+};
+
+
+int main()
+{
+	SingleThreadQueue<int> s;
+	for (int i = 0; i < 10; i++)
+	{
+		s.push(i);
+	}
+	for (int i = 0; i < 11; i++)
+	{
+		shared_ptr<int> p = s.try_pop();
+		if(p)
+		cout << *p << endl;
+	}
+}
